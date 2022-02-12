e662f885065d1a329539311b6cc10eac7bc3645d
Aleksey Yrganov <80423637+AlexLito666@users.noreply.github.com>
2022-02-12 14:55:33 +0200

    Update ping.py

--- ping.py
+++ ping.py
@@ -6,6 +6,24 @@ display.set_caption("Ping-pong")
 window = display.set_mode((700, 500))
 background = transform.scale(image.load("fon.jpg"), (win_width, win_height))
 
+class GameSprite(sprite.Sprite):
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+  # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 # переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
 finish = False
@@ -16,7 +34,7 @@ while game:
     # событие нажатия на кнопку Закрыть
     for e in event.get():
         if e.type == QUIT:
-            game = False
+            game = False 
 
 
         display.update()
